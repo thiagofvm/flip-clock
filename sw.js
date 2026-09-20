3adf4136b784d3d046e401ee5afeda170ead6d8a
thiagofvm <thiagofvm@gmail.com>
2026-09-20 18:23:55 +1000

    Delete sw.js
    improving screen saver mode

--- sw.js
+++
@@ -1,4 +0,0 @@
-const C='flipclock-v2',F=['./','index.html','icon.png'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(F)))});
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(C).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request))));
